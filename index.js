0082fd5e2fa0ecda87f2612fe373b4bf89c19ab2
Marcio Oliveira <ponkan.investimentos@gmail.com>
2019-05-06 01:58:49 -0300

    arquivo inicial completado com sucesso, realizando upload para git. Recebimento de dados do usuário

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const readline = require('readline-sync');
+
 function start(){
 	const  content ={};
 	content.searchTerm = askAndReturnSearchTerm();
-
+	content.prefix = askAndReturnPrefix();
 
 	function askAndReturnSearchTerm(){
 
-		return 'TERM EXAMPLE';
+		return readline.question('Qual termo da pesquisa? ');
+	}
+
+	function askAndReturnPrefix(){
+		const prefixes = ['quem é', 'o que é', 'a historia de '];
+		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha um dos prefixos:');
+		const selectedPrefixText = prefixes[selectedPrefixIndex];
+		
+		return (selectedPrefixText);
 	}
 
 	console.log(content);
